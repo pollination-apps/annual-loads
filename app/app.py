273c0192ed6fa51e6be26e06a392464f28ef63c6
Chris Mackey <chris@ladybug.tools>
2022-07-05 16:33:11 -0400

    feat(result): Add tables for total loads by term and Room Summary

--- app/app.py
+++ app/app.py
@@ -19,6 +19,7 @@ from honeybee_energy.run import prepare_idf_for_simulation, output_energyplus_fi
 from honeybee_energy.writer import energyplus_idf_version
 from honeybee_energy.config import folders as energy_folders
 
+from pandas import DataFrame
 import streamlit as st
 from pollination_streamlit_io import get_host
 from handlers import initialize, get_inputs
@@ -97,7 +98,7 @@ def run_idf(idf_file_path, epw_file_path=None, expand_objects=True):
     return output_energyplus_files(directory)
 
 
-def data_to_load_intensity(data_colls, floor_area, data_type, mults=None):
+def data_to_load_intensity(room_dict, data_colls, floor_area, data_type, mults=None):
     """Convert data collections from EnergyPlus to a single load intensity collection.
 
     Args:
@@ -108,8 +109,25 @@ def data_to_load_intensity(data_colls, floor_area, data_type, mults=None):
             the multipliers of the honeybee Rooms.
     """
     if len(data_colls) != 0:
+        # first try adding the data to the room dictionary
+        rel_key = 'Zone' if 'Zone' in data_colls[0].header.metadata else 'System'
+        for dat in data_colls:
+            try:
+                z_id = dat.header.metadata[rel_key]
+                if rel_key == 'Zone':
+                    r_prop = room_dict[z_id]
+                elif ' IDEAL LOADS AIR SYSTEM' in z_id:  # E+ HVAC Templates
+                    r_prop = room_dict[z_id.split(' IDEAL LOADS AIR SYSTEM')[0]]
+                elif '..' in z_id:  # convention used for service hot water
+                    r_prop = room_dict[z_id.split('..')[-1]]
+                else:
+                    r_prop = room_dict[z_id]
+                r_prop[-1][data_type] = (dat.total * r_prop[2]) / r_prop[1]
+            except KeyError:  # no results of this type for the Room
+                pass
+        # next, build up the monthly collection of total values
         if mults is not None:
-            if 'Zone' in data_colls[0].header.metadata:
+            if rel_key == 'Zone':
                 rel_mults = [mults[data.header.metadata['Zone']] for data in data_colls]
                 data_colls = [dat * mul for dat, mul in zip(data_colls, rel_mults)]
         total_vals = [sum(month_vals) / floor_area for month_vals in zip(*data_colls)]
@@ -129,8 +147,12 @@ def load_sql_data(sql_path, model):
     """
     # load up the floor area, get the model units, and the room multipliers
     con_fac = conversion_factor_to_meters(model.units) ** 2
-    floor_areas = [room.floor_area * room.multiplier * con_fac for room in model.rooms
-                   if not room.exclude_floor_area]
+    floor_areas, rd = [], {}
+    for room in model.rooms:
+        if not room.exclude_floor_area:
+            fa = room.floor_area * room.multiplier * con_fac
+            floor_areas.append(fa)
+            rd[room.identifier.upper()] = [room.display_name, fa, room.multiplier, {}]
     floor_area = sum(floor_areas)
     assert floor_area != 0, 'Model has no floors with which to compute EUI.'
     mults = {rm.identifier.upper(): rm.multiplier for rm in model.rooms}
@@ -147,11 +169,12 @@ def load_sql_data(sql_path, model):
     process2_init = sql_obj.data_collections_by_output_name(process2_out)
     shw_init = sql_obj.data_collections_by_output_name(shw_out)
 
-    # convert the results to EUI and output them
-    cooling = data_to_load_intensity(cool_init, floor_area, 'Cooling')
-    heating = data_to_load_intensity(heat_init, floor_area, 'Heating')
-    lighting = data_to_load_intensity(light_init, floor_area, 'Lighting', mults)
-    equip = data_to_load_intensity(elec_eq_init, floor_area, 'Electric Equipment', mults)
+    # convert the results to a single monthly EUI data collection
+    cooling = data_to_load_intensity(rd, cool_init, floor_area, 'Cooling')
+    heating = data_to_load_intensity(rd, heat_init, floor_area, 'Heating')
+    lighting = data_to_load_intensity(rd, light_init, floor_area, 'Lighting', mults)
+    equip = data_to_load_intensity(
+        rd, elec_eq_init, floor_area, 'Electric Equipment', mults)
     load_terms = [cooling, heating, lighting, equip]
     load_colors = [
         Color(4, 25, 145), Color(153, 16, 0), Color(255, 255, 0), Color(255, 121, 0)
@@ -160,14 +183,16 @@ def load_sql_data(sql_path, model):
     # add gas equipment if it is there
     if len(gas_equip_init) != 0:
         gas_equip = data_to_load_intensity(
-            gas_equip_init, floor_area, 'Gas Equipment', mults)
+            rd, gas_equip_init, floor_area, 'Gas Equipment', mults)
         load_terms.append(gas_equip)
         load_colors.append(Color(255, 219, 128))
     # add process load if it is there
     process = []
     if len(process1_init) != 0:
-        process1 = data_to_load_intensity(process1_init, floor_area, 'Process', mults)
-        process2 = data_to_load_intensity(process2_init, floor_area, 'Process', mults)
+        process1 = data_to_load_intensity(
+            rd, process1_init, floor_area, 'Process', mults)
+        process2 = data_to_load_intensity(
+            rd, process2_init, floor_area, 'Process', mults)
         process = process1 + process2
         load_terms.append(process)
         load_colors.append(Color(135, 135, 135))
@@ -175,7 +200,7 @@ def load_sql_data(sql_path, model):
     hot_water = []
     if len(shw_init) != 0:
         hot_water = data_to_load_intensity(
-            shw_init, floor_area, 'Service Hot Water', mults)
+            rd, shw_init, floor_area, 'Service Hot Water', mults)
         load_terms.append(hot_water)
         load_colors.append(Color(255, 0, 0))
 
@@ -185,7 +210,8 @@ def load_sql_data(sql_path, model):
 
     # return a dictionary containing all relevant results of the simulation
     return {
-        'floor_areas': floor_areas,
+        'room_results': rd,
+        'floor_area': floor_area,
         'load_terms': load_terms,
         'load_colors': load_colors,
         'balance': balance
@@ -272,7 +298,8 @@ def create_charts(container, sql_results, heat_cop, cool_cop, ip_units, normaliz
     load_terms = sql_results['load_terms'].copy()
     load_colors = sql_results['load_colors']
     balance = sql_results['balance'].copy()
-    floor_areas = sql_results['floor_areas']
+    room_results = sql_results['room_results']
+    floor_area = sql_results['floor_area']
 
     # convert the data to the correct units system
     display_units = 'kBtu/ft2' if ip_units else 'kWh/m2'
@@ -286,9 +313,9 @@ def create_charts(container, sql_results, heat_cop, cool_cop, ip_units, normaliz
     # total the data over the floor area if normalize is false
     if not normalize:
         if ip_units:
-            display_units, a_unit, total_area = 'kBtu', 'ft2', sum(floor_areas) * 10.7639
+            display_units, a_unit, total_area = 'kBtu', 'ft2', floor_area * 10.7639
         else:
-            display_units, a_unit, total_area = 'kWh', 'm2', sum(floor_areas)
+            display_units, a_unit, total_area = 'kWh', 'm2', floor_area
         load_terms = [data.aggregate_by_area(total_area, a_unit) for data in load_terms]
         balance = [data.aggregate_by_area(total_area, a_unit) for data in balance]
 
@@ -298,16 +325,21 @@ def create_charts(container, sql_results, heat_cop, cool_cop, ip_units, normaliz
     if heat_cop != 1:
         load_terms[1] = load_terms[1] / heat_cop
 
-    # report the total load intensity
+    # report the total load and the breakdown into different terms
     total_load = [dat.total for dat in load_terms]
     container.subheader(
-        'Total Load Intensity: {} {}'.format(round(sum(total_load), 2), display_units))
+        'Total Load: {:,.2f} {}'.format(sum(total_load), display_units))
+
+    # create a table with totaled data
+    total_data = {d.header.metadata['type']: [d.total] for d in load_terms}
+    tot_df = DataFrame(total_data)
+    container.dataframe(tot_df.style.format('{:,.2f}'))
 
     # plot the monthly data collections on a bar chart
     leg_par = LegendParameters(colors=load_colors)
     leg_par.decimal_count = 0
     month_chart = MonthlyChart(load_terms, leg_par, stack=True)
-    figure = month_chart.plot(title='Load Intensity')
+    figure = month_chart.plot(title='Monthly Load')
     container.plotly_chart(figure)
 
     # create a monthly chart with the load balance
@@ -315,8 +347,42 @@ def create_charts(container, sql_results, heat_cop, cool_cop, ip_units, normaliz
     leg_par = LegendParameters(colors=bal_colors)
     leg_par.decimal_count = 0
     month_chart = MonthlyChart(balance, leg_par, stack=True)
-    figure = month_chart.plot(title='Load Balance')
+    figure = month_chart.plot(title='Monthly Load Balance')
     container.plotly_chart(figure)
+
+    # process all of the detailed room results into a table
+    container.write('Room Summary ({})'.format(display_units))
+    table_data = {'Room': []}
+    load_types = [dat.header.metadata['type'] for dat in load_terms]
+    for lt in load_types:
+        table_data[lt] = []
+    for room_data in room_results.values():
+        name, fa, mult, res = room_data
+        table_data['Room'].append(name)
+        for lt in load_types:
+            try:
+                val = res[lt] if normalize else res[lt] * fa * mult
+                table_data[lt].append(val)
+            except KeyError:
+                table_data[lt].append(0.0)
+    # perform any unit conversions on the table data
+    if ip_units:
+        conv = 0.316998 if normalize else 3.41214
+        for col, dat in table_data.items():
+            if col != 'Room':
+                table_data[col] = [val * conv for val in table_data[col]]
+    if cool_cop != 1:
+        table_data['Cooling'] = [val / cool_cop for val in table_data['Cooling']]
+    if heat_cop != 1:
+        table_data['Heating'] = [val / heat_cop for val in table_data['Heating']]
+    # add a column for the total data of each room
+    totals = [0] * len(table_data['Cooling'])
+    for col, dat in table_data.items():
+        if col != 'Room':
+            for i, v in enumerate(dat):
+                totals[i] += v
+    table_data['Total'] = totals
+    container.dataframe(table_data)
 
 
 def main(platform):
